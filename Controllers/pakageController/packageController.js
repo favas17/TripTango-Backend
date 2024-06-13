715ef57b6adca58ae29a43caf2b13a1928d9c67f
Muahmmed Favas <favasfvs111@gmail.com>
2024-06-13 05:50:19 -0700

    changes

--- Controllers/pakageController/packageController.js
+++ Controllers/pakageController/packageController.js
@@ -3,8 +3,14 @@ const packageModel = require("../../Models/agentModel.js/packageModel")
 
 
 const packageGet = async (req,res)=>{
-    const packages = await packageModel.find();
-    return res.status(200).json({message:"fetched succesully"})
+
+    try{
+        const packages = await packageModel.find();
+        return res.status(200).json(packages);
+    }catch(error){
+        return res.status(500).json({message:'Failed to fetch package details',error:error.message})
+    }
+
 }
 
 // adding package
@@ -14,9 +20,8 @@ const packagePost = async (req,res,next)=>{
     const {packageName,location,price,details,duration,mapLocation,category} = req.body;
     const days = JSON.parse(req.body.days);
     // mapping throug images files
-    const images = req.files.map(file =>file.path);
-    
-
+    const images = req.files.map(file =>file.filename);
+console.log(images)
     const newPackage = new packageModel({
         packageName,
         location,
